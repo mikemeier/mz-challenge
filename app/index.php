b41e4c8d54d3d0603d35ceae092808cdb65a930b
mario zingg <mario.zingg@bluewin.ch>
2020-08-03 17:00:10 +0200

    use alias for simplify code

--- app/index.php
+++ app/index.php
@@ -1,14 +1,17 @@
 <?php
 
+use MzChallenge\ControllerExecuter;
+use MzChallenge\ControllerResolver;
+
 error_reporting(E_ALL);
 ini_set('display_errors', true);
 
 require_once __DIR__.'/vendor/autoload.php';
 
 $requestUri = $_SERVER['REQUEST_URI'];
-$controllerResolver = new \MzChallenge\ControllerResolver($requestUri);
+$controllerResolver = new ControllerResolver($requestUri);
 
 $controllerAndAction = $controllerResolver->getControllerAndAction();
 
-$executer = new \MzChallenge\ControllerExecuter($controllerAndAction);
+$executer = new ControllerExecuter($controllerAndAction);
 $executer->execution();
